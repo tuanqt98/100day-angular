dbbfeb2919a4183719f1798d54bc78eaba9b5569
DC-VIETIS\TuanNQ <tuannq35@vietis.com.vn>
2022-11-07 10:59:48 +0700

    update main

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TodotoComponent } from './todoto/todoto.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TodotoComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
